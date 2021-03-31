86a93da3b87436d8b7fb27d3648a2d4a260192ba
Karel Dohnal <karel.dohnal@gmail.com>
2021-03-31 16:28:23 +0200

    initial react app

--- server/engine/engineController.js
+++ server/engine/engineController.js
@@ -4,7 +4,8 @@ export default app => {
   const io = new Server(app, {});
 
   io.on('connection', socket => {
-    logger.info('Client Connected');
+    logger.info('Client Connected:');
+    logger.info(socket);
   });
 
   return io;
