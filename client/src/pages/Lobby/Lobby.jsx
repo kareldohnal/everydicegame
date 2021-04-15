a6e53832e72b4590a87196bc73cd202f1a3a04a9
Karel Dohnal <karel.dohnal@gmail.com>
2021-04-15 14:31:49 +0200

    basic tabs landing page

--- client/src/pages/Lobby/Lobby.jsx
+++ client/src/pages/Lobby/Lobby.jsx
@@ -4,21 +4,6 @@ import { Nav } from '../../components/Nav/Nav';
 // import './Lobby.scss';
 
 export const Lobby = () => {
-    const [game, setGame] = useState('dice');
-    const [roomID, setRoomID] = useState('pica');
-
-    const handleChange = (e) => {
-        setGame(e.target.value)
-    }
-    const handleChangeRoom = (e) => {
-        setRoomID(e.target.value)
-    }
-
-    const handleSubmit = (e) => {
-        alert('Your favorite flavor is: ' + game + roomID);
-        e.preventDefault();
-    }
-
     const [tab, setTab] = useState('game-tab');
 
     const handleTab = (e) => {
@@ -30,22 +15,18 @@ export const Lobby = () => {
             <div className="lobby">
                 <Nav />
                 <div className="tabs">
-                    <button id='game-tab' className={'game-tab' === tab ? 'tab-item active' : 'tab-item'}onClick={handleTab}>London</button>
-                    <button id='sim-tab' className={'sim-tab' === tab ? 'tab-item active' : 'tab-item'} onClick={handleTab}>Paris</button>
+                    <button id='game-tab' className={'game-tab' === tab ? 'tab-item active' : 'tab-item'} onClick={handleTab}>Game rooms</button>
+                    <button id='sim-tab' className={'sim-tab' === tab ? 'tab-item active' : 'tab-item'} onClick={handleTab}>Dice simulator</button>
                 </div>
-                Create game :
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Pick your game:
-                        <select value={game} onChange={handleChange}>
-                            <option value="dice">Dice1</option>
-                            <option value="dice2">Dice2</option>
-                            <option value="dice3">Dice3</option>
-                        </select>
-                        <input type="text" id="roomID" name="roomID" value={roomID} onChange={handleChangeRoom} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
+
+                {tab === 'game-tab' && (
+                    <div>hra</div>
+                )}
+
+                {tab === 'sim-tab' && (
+                    <div>simulátor</div>
+                )}
+
             </div>
         </>
     );
